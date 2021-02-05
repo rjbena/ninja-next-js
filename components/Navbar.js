95b707ad4ff0539934956bf959df8584dcc13104
ricky <rjbenavides@cpp.edu>
2021-02-05 09:34:54 -0800

    Added Navbar and Footer component.

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className="logo">
+      <h1>Ninja List</h1>
+      <a>Home</a>
+      <a>About</a>
+      <a>Ninja Listing</a>
+    </nav>
+  );
+};
+
+export default Navbar;
