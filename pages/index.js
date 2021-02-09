ddc8316179ecd6c652d42c2c58e3bdabb27a0db8
ricky <rjbenavides@cpp.edu>
2021-02-09 15:46:57 -0800

    Added styles

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
@@ -6,13 +7,16 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis
         voluptatum, quidem illo obcaecati id aliquid voluptates consequuntur
         odit doloremque dolorum eum explicabo repellendus odio corrupti? Ullam
         officiis ratione ad accusamus.
       </p>
+      <Link href="/ninjas">
+        <a className={styles.btn}>See Ninja Listing</a>
+      </Link>
     </div>
   );
 }
