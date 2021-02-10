3d1576b7e3ac9e9a212758e2c42eb242a2e8c2e0
ricky <rjbenavides@cpp.edu>
2021-02-10 14:50:26 -0800

    Added ninja list page with styles.

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,29 @@
-const Ninjas = () => {
-  return <h1>Ninjas</h1>;
+import styles from "../../styles/Ninjas.module.css";
+
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div>
+      <h1>Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single} href="">
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: {
+      ninjas: data,
+    },
+  };
 };
 
 export default Ninjas;
